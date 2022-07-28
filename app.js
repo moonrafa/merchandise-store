d720a7765de9c092e8240eaf584e8b255c3bdc09
Rafa <rrafasrodrigues@gmail.com>
2022-07-28 20:06:24 -0300

    via vscode

---
+++ app.js
@@ -0,0 +1,19 @@
+//show cart
+//immediately invoked function
+
+;(function () {
+  const cartInfo = document.getElementById('cart-info')
+  const cart = document.getElementById('cart')
+  cartInfo.addEventListener('click', function () {
+    cart.classList.toggle('show-cart')
+  })
+})()
+//add items to the cart
+;(function () {
+  const cartBtn = document.querySelectorAll('.store-item-icon')
+  cartBtn.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      //console.log(e.target)
+    })
+  })
+})()

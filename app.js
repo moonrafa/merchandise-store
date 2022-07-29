a6cdcb1cb7dc2fd576df7ae906b2084133d105e5
Rafa <rrafasrodrigues@gmail.com>
2022-07-29 11:16:11 -0300

    <>

--- app.js
+++ app.js
@@ -13,7 +13,59 @@
   const cartBtn = document.querySelectorAll('.store-item-icon')
   cartBtn.forEach(function (btn) {
     btn.addEventListener('click', function (e) {
-      //console.log(e.target)
+      if (e.target.parentElement.classList.contains('store-item-icon')) {
+        let fullPath = e.target.parentElement.previousElementSibling.src
+        const item = {}
+        item.img = fullPath
+        let name =
+          e.target.parentElement.parentElement.nextElementSibling.children[0]
+            .children[0].textContent
+        item.name = name
+        let price =
+          e.target.parentElement.parentElement.nextElementSibling.children[0]
+            .children[1].textContent
+        let finalPrice = price.slice(2).trim()
+        item.price = finalPrice
+        const cartItem = document.createElement('div')
+        cartItem.classList.add(
+          'cart-item',
+          'd-flex',
+          'justify-content-between',
+          'text-capitalize',
+          'my-3'
+        )
+        cartItem.innerHTML = `
+        <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+        <div class="cart-item-text">
+          <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+          <span>R$</span>
+          <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+        </div>
+        <a href="#" id='cart-item-remove' class="cart-item-remove">
+          <i class="fas fa-trash"></i>
+        </a>
+      </div>`
+        const cart = document.getElementById('cart')
+        const total = document.querySelector('.cart-total-container')
+        cart.insertBefore(cartItem, total)
+        alert('item added to the cart')
+        showTotal()
+      }
     })
   })
+  function showTotal() {
+    const total = []
+    const items = document.querySelectorAll('.cart-item-price')
+    items.forEach(function (item) {
+      total.push(parseFloat(item.textContent))
+    })
+    const totalMoney = total.reduce(function (total, item) {
+      total += item
+      return total
+    }, 0)
+    const finalMoney = totalMoney.toFixed(2)
+    document.getElementById('cart-total').textContent = finalMoney
+    document.getElementById('item-count').textContent = total.length
+    document.querySelector('.item-total').textContent = finalMoney
+  }
 })()
